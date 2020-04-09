2587772e3101c39d2951a68fa7c907691557ae10
Nauara Melo <nauaramelo@gmail.com>
2020-04-09 16:08:31 -0300

    Planner

---
+++ semana12/planner/build/precache-manifest.75d9b93e70dc78031865a00bf7825bb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5ccb87ea5af5696d59bbc593bb66ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3856c5111734dce49731",
+    "url": "/static/js/2.eb3e672f.chunk.js"
+  },
+  {
+    "revision": "11a643f867b62cde689f",
+    "url": "/static/js/main.4e02b238.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8664bc93c51af1547493d0e9018ab61d",
+    "url": "/static/media/foto2.8664bc93.png"
+  }
+]);
